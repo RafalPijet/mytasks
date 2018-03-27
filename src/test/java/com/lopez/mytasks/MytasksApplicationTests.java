78be1883a6b740a3526e32834570edbbd295fca5
Rafal Pijet <rafalpijet@iMac-RafaPijet.local>
2018-03-27 22:22:02 +0200

    Third Commit

--- src/test/java/com/lopez/mytasks/MytasksApplicationTests.java
+++ src/test/java/com/lopez/mytasks/MytasksApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lopez.mytasks;
+/*package com.lopez.mytasks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class MytasksApplicationTests {
 	}
 
 }
+*/
